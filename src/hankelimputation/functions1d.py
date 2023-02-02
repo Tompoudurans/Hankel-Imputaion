76e473786f54c501298f7c0391965b38c22333e9
thomas poudevigne-durance <drtrom95@gmail.com>
2023-02-02 11:10:31 +0000

    add docsting2

--- src/hankelimputation/functions1d.py
+++ src/hankelimputation/functions1d.py
@@ -3,7 +3,9 @@ import numpy as np
 
 def hankel_imputaion_1d(data,lag,e,mask,maxinter):
     """
-
+    Preforms a hankel imputaion with 1 dimetions
+    by a convex optimation of minimation of the norm of hankle matrix of imputed variables
+    with constraints = norm of mask of data and imputed variables less than a value e
     """
     N=len(data)
     Yapp = cp.Variable(N)
@@ -14,6 +16,9 @@ def hankel_imputaion_1d(data,lag,e,mask,maxinter):
     return Yapp.value
 
 def cphanker(row,reman):
+    """
+    creates a hankel matix with one dimetions
+    """
     hank = []
     xi = row.shape[0]
     for i in range(xi):
